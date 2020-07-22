42078facedf6df4f471adaedf4d5bd65d5b30b47
codeminnam <code.minnam@gmail.com>
2020-07-22 18:57:11 +0900

    fix: seperate utils by state

--- src/lib/asyncUtils.js
+++ src/lib/asyncUtils.js
@@ -1,30 +1,80 @@
-export const reducerUtils = {
+export const reducerPageUtils = {
     initial: (data = null) => ({
         loading: false,
         data,
         error: null,
-        length: null,
         page: 1
     }),
     loading: (prevState = null) => ({
         loading: true,
         data: prevState,
         error: null,
-        length: null,
         page: prevState
     }),
     success: payload => ({
         loading: false,
-        data: payload,
+        data: payload.pageComments,
         error: null,
-        length: payload.length,
         page: payload.page
     }),
     error: error => ({
         loading: false,
         data: null,
         error: error,
-        length: null,
+        page: 1
+    })
+}
+
+export const reducerCommentsUtils = {
+    initial: (data = null) => ({
+        loading: false,
+        data,
+        error: null,
+        length: null
+    }),
+    loading: (prevState = null) => ({
+        loading: true,
+        data: prevState,
+        error: null,
+        length: prevState
+    }),
+    success: (prevState = null, payload) => ({
+        loading: false,
+        data: payload,
+        error: null,
+        length: ((prevState === payload.length) ? prevState : payload.length)
+    }),
+    error: error => ({
+        loading: false,
+        data: null,
+        error: error,
+        length: null
+    })
+}
+
+export const reducerCommentUtils = {
+    initial: (data = null) => ({
+        loading: false,
+        data,
+        error: null,
+        page: null
+    }),
+    loading: (prevState = null) => ({
+        loading: true,
+        data: prevState,
+        error: null,
+        page: prevState
+    }),
+    success: payload => ({
+        loading: false,
+        data: payload,
+        error: null,
+        page: payload.page
+    }),
+    error: error => ({
+        loading: false,
+        data: null,
+        error: error,
         page: 1
     })
 }
