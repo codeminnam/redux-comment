5208e6c34f33b4a792577ba250c8b5df64c8dd39
codeminnam <code.minnam@gmail.com>
2020-10-16 21:23:50 +0900

    style: Style the page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import './App.css';
+import styled from "styled-components";
 import CommentListContainer from './containers/CommentListContainer';
 import PageListContainer from './containers/PageListContainer';
 import FormContainer from './containers/FormContainer';
 
-function App(){
+function App() {
 
-    return (
-      <div>
-          <CommentListContainer />
-          <PageListContainer />
-          <FormContainer />
-      </div>
-    )
+  const AppContainer = styled.div`
+    border: 1px solid lightgray;
+    background-color: #fff;
+  `;
+
+  const Header = styled.div`
+    border-bottom: 1px solid lightgray;
+    padding: 20px;
+  `;
+
+  const ListContainer = styled.div`
+    padding: 20px;
+  `;
+
+  return (
+    <AppContainer>
+      <Header>
+        <strong>Comment Page</strong>
+      </Header>
+      <ListContainer>
+        <CommentListContainer />
+        <PageListContainer />
+      </ListContainer>
+      <FormContainer />
+    </AppContainer>
+  )
 }
 
 export default App;
